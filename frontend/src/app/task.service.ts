19d29823f4cae1867a085cd9befa1b9b759c16b4
ManassesM <manamiui2001@gmail.com>
2021-01-28 15:07:49 -0300

    new-task component, task module and list module were created

--- frontend/src/app/task.service.ts
+++ frontend/src/app/task.service.ts
@@ -8,17 +8,22 @@ export class TaskService {
 
   constructor(private webRequestService: WebRequestService) { }
 
+  getLists() {
+    return this.webRequestService.get('lists');
+  }
+
   createList(title: string) {
     // We want to send a web request to create a list
     return this.webRequestService.post('lists', { title });
   }
 
-  getLists() {
-    return this.webRequestService.get('lists');
-  }
-
   getTasks(listId: string) {
     return this.webRequestService.get(`lists/${listId}/tasks`);
+  }
+
+  createTask(title: string, listId: string) {
+    // We want to send a web request to create a task
+    return this.webRequestService.post(`lists/${listId}/tasks`, { title });
   }
 
 }
